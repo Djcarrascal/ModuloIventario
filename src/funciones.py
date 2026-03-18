c47d6f9b2a6b0cf0430fd4f9c5aabee3dbae2ebd
David Carrascal <d8carrascal@gmail.com>
2026-03-18 15:53:42 -0500

    Hice cambios en una nueva rama

---
+++ src/funciones.py
@@ -0,0 +1,41 @@
+def agregar_producto():
+    # Solicitamos los datos requeridos y colocamos condiciones para que los datos ingresados sean validos para el sistema
+        while True:
+                try:
+                        nombre = str(input("\nIngrese el nombre del producto: "))
+        
+                        if not nombre:
+                                print("\nNo puede dejar el nombre vacío, por favor ingrese el nombre")
+                                continue
+                        elif not nombre.isalpha():
+                                print("\nERROR - Solo se pueden ingresar letras")
+                                continue
+                        break
+                except:
+                        nombre = input("\nIngrese el nombre del producto: ")
+                        continue
+
+        while True:
+                try:
+                        cantidad = int(input("\nIngrese la cantidad: "))
+                        if not cantidad:
+                                print("\nNo puede dejar el nombre vacío, por favor ingrese la cantidad ")
+                        break
+                except ValueError:
+                        print("\nSolo de pueden ingresar numeros")                
+
+        while True:
+                try:
+                        precio = int(input("\nIngrese el precio del producto: "))
+                        if not precio:
+                                print("\nNo puede dejar el precio vacío, por favor ingrese el precio")
+                                continue
+                        break
+                except ValueError:
+                        print("\nSolo de pueden ingresar numeros")
+
+        costo_total = precio * cantidad
+        print(f"\nProducto ingresado: {nombre}, cantidad: {cantidad}, precio unitario: {precio} y el costo total ingresado es de: {costo_total}")
+
+
+               
--- src/inventario.py
+++ src/inventario.py
@@ -1,51 +1,26 @@
+from funciones import agregar_producto
+opcion = 1
 # Creamos un menú de opciones para el usuario
-print("\nInventario de Productos\n")
 
-print("Menu de opciones:\n")
-print("1. Agregar Productos")
-print("2. Ver Productos")
-print("3. Actualizar Producto Existente")
-print("4. Eliminar Producto")
-print("0. Salir")
 
-opcion = input("\nIngrese una opcion: ")
-if opcion == "1":
+# Aqui definimos que el usuario ingrese una opcion y la vez que solo sea una opcion valida
 
-# Solicitamos los datos requeridos y colocamos condiciones para que los datos ingresados sean validos para el sistema
-        while True:
-                try:
-                        nombre = str(input("\nIngrese el nombre del producto: "))
-        
-                        if not nombre:
-                                print("\nNo puede dejar el nombre vacío, por favor ingrese el nombre")
-                                continue
-                        elif not nombre.isalpha():
-                                print("\nERROR - Solo se pueden ingresar letras")
-                                continue
-                        break
-                except:
-                        nombre = input("\nIngrese el nombre del producto: ")
-                        continue
 
-        while True:
-                try:
-                        cantidad = int(input("\nIngrese la cantidad: "))
-                        if not cantidad:
-                                print("\nNo puede dejar el nombre vacío, por favor ingrese la cantidad ")
-                        break
-                except ValueError:
-                        print("\nSolo de pueden ingresar numeros")                
-
-        while True:
-                try:
-                        precio = int(input("\nIngrese el precio del producto: "))
-                        if not precio:
-                                print("\nNo puede dejar el precio vacío, por favor ingrese el precio")
-                                continue
-                        break
-                except ValueError:
-                        print("\nSolo de pueden ingresar numeros")
-
-costo_total = precio * cantidad
-print(f"\nProducto ingresado: {nombre}, cantidad: {cantidad}, precio unitario: {precio} y el costo total ingresado es de: {costo_total}")
-
+while opcion != 0:
+    print("\nInventario de Productos\n")
+    print("Menu de opciones:\n")
+    print("1. Agregar Productos")
+    print("2. Ver Productos")
+    print("3. Actualizar Producto Existente")
+    print("4. Eliminar Producto")
+    print("0. Salir")
+    while True:
+        try:
+            opcion = int(input("\nIngrese una opción: "))
+            break
+        except ValueError:
+                    print("Por favor ingrese un número")
+             
+    if opcion == 1:
+            agregar_producto()
+    print("Opcion invalida.")
