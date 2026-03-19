89427e9c92179d264caf9415bc3cfdfd18976f52
David Carrascal <d8carrascal@gmail.com>
2026-03-19 15:28:34 -0500

    Agregué nuevas funciones

--- src/funciones.py
+++ src/funciones.py
@@ -1,41 +1,60 @@
+producto_nuevo = []
+
+#Esta funcion permite agregar un producto digitando solo valores válidos y en caso del ingreso de algo no permitido el programa solicita nuevamente la información.
 def agregar_producto():
+
     # Solicitamos los datos requeridos y colocamos condiciones para que los datos ingresados sean validos para el sistema
-        while True:
-                try:
-                        nombre = str(input("\nIngrese el nombre del producto: "))
-        
-                        if not nombre:
-                                print("\nNo puede dejar el nombre vacío, por favor ingrese el nombre")
-                                continue
-                        elif not nombre.isalpha():
-                                print("\nERROR - Solo se pueden ingresar letras")
-                                continue
-                        break
-                except:
-                        nombre = input("\nIngrese el nombre del producto: ")
-                        continue
+    while True:
+        try:
+            nombre = str(input("\nIngrese el nombre del producto: "))
+            if not nombre:
+                    print("\nNo puede dejar el nombre vacío, por favor ingrese el nombre")
+                    continue
+            elif not nombre.isalpha():
+                    print("\nERROR - Solo se pueden ingresar letras")
+                    continue
+            break
+        except:
+            nombre = input("\nIngrese el nombre del producto: ")
+            continue
+    while True:
+        try:
+            cantidad = int(input("\nIngrese la cantidad: "))
+            if not cantidad:
+                    print("\nNo puede dejar el nombre vacío, por favor ingrese la cantidad ")
+            break
+        except ValueError:
+            print("\nSolo de pueden ingresar numeros")                
+    while True:
+        try:
+            precio = int(input("\nIngrese el precio del producto: "))
+            if not precio:
+                    print("\nNo puede dejar el precio vacío, por favor ingrese el precio")
+                    continue
+            break
+        except ValueError:
+            print("\nSolo de pueden ingresar numeros")
 
-        while True:
-                try:
-                        cantidad = int(input("\nIngrese la cantidad: "))
-                        if not cantidad:
-                                print("\nNo puede dejar el nombre vacío, por favor ingrese la cantidad ")
-                        break
-                except ValueError:
-                        print("\nSolo de pueden ingresar numeros")                
+#Aquí creé el diccionario que permitirá almacenar los prodcutos que digite el usuario.
+    producto = {
+               "nombre":nombre,
+               "precio":precio,
+               "cantidad": cantidad,
+               "costo_total" : precio*cantidad
+        }
 
-        while True:
-                try:
-                        precio = int(input("\nIngrese el precio del producto: "))
-                        if not precio:
-                                print("\nNo puede dejar el precio vacío, por favor ingrese el precio")
-                                continue
-                        break
-                except ValueError:
-                        print("\nSolo de pueden ingresar numeros")
-
-        costo_total = precio * cantidad
-        print(f"\nProducto ingresado: {nombre}, cantidad: {cantidad}, precio unitario: {precio} y el costo total ingresado es de: {costo_total}")
+    producto_nuevo.append(producto)
 
 
-               
+
+    print(f"\nProducto ingresado: {nombre}, cantidad: {cantidad}, precio unitario: {precio} y el costo total ingresado es de: {precio*cantidad}")
+
+#En esta funcion tomé datos de la funcion (agregar_producto) para hacer el cálculo de los totales de cantidad y precio solicitados sumando item por item.
+def calcular_estadisticas():
+        total_cantidad = sum(item["cantidad"]for item in producto_nuevo)
+        total_costo = sum(item["costo_total"]for item in producto_nuevo)
+        print(f"\nSu inventario tiene {total_cantidad} productos, con un valor total de ${total_costo} USD")
+
+#Creé una funcion de mensaje para que solamente llamarla depues de finalizar cada opcion elegida del menu principal
+def mensaje_agradecimiento():
+        print("\nGracias por usar el módulo de inventario")
--- src/inventario.py
+++ src/inventario.py
@@ -1,26 +1,37 @@
-from funciones import agregar_producto
+from funciones import *
 opcion = 1
-# Creamos un menú de opciones para el usuario
 
-
-# Aqui definimos que el usuario ingrese una opcion y la vez que solo sea una opcion valida
-
-
+# Creamos un menú de opciones para el usuario y creamos un bucle. Tambien definimos que el usuario solo pueda ingresar opciones validas.
 while opcion != 0:
     print("\nInventario de Productos\n")
     print("Menu de opciones:\n")
     print("1. Agregar Productos")
     print("2. Ver Productos")
-    print("3. Actualizar Producto Existente")
-    print("4. Eliminar Producto")
+    print("3. Calcular estadísticas")
     print("0. Salir")
     while True:
         try:
             opcion = int(input("\nIngrese una opción: "))
             break
         except ValueError:
-                    print("Por favor ingrese un número")
+                    print("Por favor ingrese una opción válida")
              
     if opcion == 1:
             agregar_producto()
-    print("Opcion invalida.")
+            mensaje_agradecimiento()
+
+    elif opcion == 2:
+          if not producto_nuevo:
+                print("\nEl inventario está vacío.")
+          else:
+                print(producto_nuevo)
+                
+    elif opcion == 3:
+            calcular_estadisticas()
+            mensaje_agradecimiento()
+    
+    else:
+          mensaje_agradecimiento()
+            
+          
+          
